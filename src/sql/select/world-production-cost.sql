576417b73d4fa4eabbbdc17e43e378b0aa3550ac
gergling <gregmichaeldavies@gmail.com>
2015-10-24 16:08:45 +0100

    Initial world production cost query.

---
+++ src/sql/select/world-production-cost.sql
@@ -0,0 +1,11 @@
+﻿SELECT 
+  economy.economy_label, 
+  product.product_label, 
+  world.world_label, 
+  world_production_cost.world_product_quantity
+FROM 
+  public.economy
+left join public.product on (economy.economy_id = product.economy_id)
+left join public.world on (world.economy_id = economy.economy_id)
+left join public.world_production on (world_production.world_id = world.world_id AND world_production.product_id = product.product_id)
+left join public.world_production_cost on (world_production_cost.world_production_id = world_production.world_production_id AND world.world_id = world_production_cost.world_id AND product.product_id = world_production_cost.product_id)
