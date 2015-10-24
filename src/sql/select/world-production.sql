f13ff9605966f00ca9cae86ea5f23e18ad5a1925
gergling <gregmichaeldavies@gmail.com>
2015-10-24 15:55:51 +0100

    Query returning the production quantities for each world.

---
+++ src/sql/select/world-production.sql
@@ -0,0 +1,14 @@
+﻿SELECT 
+  economy.economy_label, 
+  product.product_label, 
+  world_production.world_product_quantity
+FROM 
+  public.economy, 
+  public.product, 
+  public.world_production, 
+  public.world
+WHERE 
+  economy.economy_id = product.economy_id AND
+  world_production.world_id = world.world_id AND
+  world_production.product_id = product.product_id AND
+  world.economy_id = economy.economy_id;
