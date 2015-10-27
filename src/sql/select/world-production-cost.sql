674ab647b97f945c2c8405deb2e4fda894fef652
gergling <gregmichaeldavies@gmail.com>
2015-10-27 09:12:34 +0000

    Included ids for troubleshooting data.

--- src/sql/select/world-production-cost.sql
+++ src/sql/select/world-production-cost.sql
@@ -1,10 +1,13 @@
 ﻿SELECT 
-  economy.economy_label, 
-  product.product_label, 
-  world.world_label, 
-  world_production_cost.world_product_quantity
-FROM 
-  public.economy
+  economy.economy_label AS economy, 
+  product.product_label AS product, 
+  world.world_label AS world,
+  world_production_cost.world_product_quantity AS quantity,
+  world_production.product_id,
+  world_production.world_production_id,
+  world.world_id,
+  product.product_id
+FROM public.economy
 left join public.product on (economy.economy_id = product.economy_id)
 left join public.world on (world.economy_id = economy.economy_id)
 left join public.world_production on (world_production.world_id = world.world_id AND world_production.product_id = product.product_id)
